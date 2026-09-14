8e0b2a30a8bdcb576cac3fb0976814f524a95d20
Atharva Shelar <23amtics404@gmail.com>
2026-09-14 14:29:21 +0530

    Update application version

--- app/server.js
+++ app/server.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello from GitHub Actions CI/CD!\n");
+  res.end("Hello from GitHub Actions CI/CD V2!\n");
 });
 
 server.listen(PORT, () => {
